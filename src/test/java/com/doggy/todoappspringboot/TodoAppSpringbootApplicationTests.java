f215e9d06d2fa58525d716b45c67a95e2141ec8f
Doggy-068 <728724654@qq.com>
2023-08-06 21:56:59 +0800

    chore: init

---
+++ src/test/java/com/doggy/todoappspringboot/TodoAppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doggy.todoappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
